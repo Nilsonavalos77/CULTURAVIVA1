0de60ccfc5c01c8601fa5f48c3e4e2d5bb86f0f0
Belen Baez <bnoeliabelen@gmail.com>
2025-07-25 13:41:46 -0300

    refactor: actualizar migraciones y eliminar campos innecesarios en el modelo de evento

--- backend/urls.py
+++ backend/urls.py
@@ -4,9 +4,9 @@ from django.http import HttpResponse
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('', lambda request: HttpResponse("¡Servidor Django funcionando! Accede a /admin/, /api/users/ o /api/eventos/"), name='home-api'), # <--- Añade esta línea
+    path('', lambda request: HttpResponse("¡Servidor Django funcionando! Accede a /admin/, /api/users/ o /api/eventos/"), name='home-api'),
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
+    path('api/users/', include('users.urls')),  # <-- ¡Aquí está la clave!
     path('api/eventos/', include('events.urls')),
     path('api/users/login/', obtain_auth_token),
 ]
