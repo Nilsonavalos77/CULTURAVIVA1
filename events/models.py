bbc4dc9d4e78165a7116b60fb10010931aef665b
Belen Baez <bnoeliabelen@gmail.com>
2025-08-06 02:57:43 -0300

    modificaciones en el formulario, para corregir errores y agregar ubicacion

--- events/models.py
+++ events/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Event(models.Model):
     titulo = models.CharField(max_length=100)
-    descripcion = models.TextField()
+    descripcion = models.TextField()  # sin tilde
+    ubicacion = models.CharField(max_length=255)  # sin tilde
     fecha = models.DateField()
     imagen = models.ImageField(upload_to='eventos/', blank=True, null=True)
-
     # Campo para registrar cuándo se creó el evento
     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
